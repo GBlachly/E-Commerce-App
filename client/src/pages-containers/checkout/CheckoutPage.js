c1a115cbc7c60594cb64028a8763486562b9e12d
GBlachly <gblachly4@gmail.com>
2023-04-05 20:13:47 -0400

    Added StripeCheckout component to checkout page

--- client/src/pages-containers/checkout/CheckoutPage.js
+++ client/src/pages-containers/checkout/CheckoutPage.js
@@ -9,6 +9,7 @@ import { checkout } from '../../store/cart/cartActions';
 import { AddAddressForm } from '../../components/addAddressForm/AddAddressForm';
 import { PickAddressForm } from '../../components/pickAddressForm/PickAddressForm';
 import { AddressCard } from '../../components/addressCard/AddressCard';
+import StripeCheckout from 'react-stripe-checkout';
 
 
 export const CheckoutPage = () => {
@@ -33,6 +34,20 @@ export const CheckoutPage = () => {
         return total;
     };
     //WILL AT TIMES HAVE A LOT OF DECIMAL PLACES, MUST STOP AT TWO
+
+
+    //STRIPE TOKEN FUNCTION
+    const onToken = (token) => {
+        fetch('/save-stripe-token', {
+          method: 'POST',
+          body: JSON.stringify(token),
+        }).then(response => {
+          response.json().then(data => {
+            alert(`We are in business, ${data.email}`);
+          });
+        });
+    };
+    //NEED TO LEARN MORE ABOUT THIS AND STRIPE IN GENERAL
 
 
     if (!loggedIn) {
@@ -82,13 +97,19 @@ export const CheckoutPage = () => {
             <h1>Checkout</h1>
             <h2>Total Price: {totalPrice()}</h2>
 
-            <div className=''>
+            <div id='checkout'>
                 <AddressCard addressId={addressId}/>
-            </div>
-
-            <div className='checkout'>
+           
+                <StripeCheckout
+                    className='mt-3'
+                    token={onToken}
+                    stripeKey="pk_test_51MqkuaGpogIECJKF83s5CMVhGSQ7wHft7QywDnDSEuUzyHnVRsBw16sK09e3KXeNXFhsruTtcrDzJZpXv8KacE2s00jGjNrJIj"
+                >
+                    <button className="btn btn-success">Pay With Card</button>
+                </StripeCheckout>
+            
                 <button 
-                className='btn btn-danger'
+                    className='mt-3 btn btn-danger'
                     onClick={ ()=>{
 
                         dispatch(checkout({
@@ -99,11 +120,9 @@ export const CheckoutPage = () => {
                         setAddressId(null);
                     } }
                 >Checkout!!!</button>
-            </div>
-
-            <div className='mt-3'>
+            
                 <button
-                    className='btn btn-secondary'
+                    className='mt-3 btn btn-secondary'
                     onClick={ () => setAddressId(null) }
                 >Go Back</button>
             </div>
